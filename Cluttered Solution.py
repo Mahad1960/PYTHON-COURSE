d387bbb9d024b11d11662def396577de0c644a6e
Mahad Shahnawaz <mahadshahnawaz72@gmail.com>
2025-07-17 00:20:18 +0500

    Delete Cluttered Solution.py

--- Cluttered Solution.py
+++
@@ -1,8 +0,0 @@
-import os
-files=os.listdir("CLUTTERED FOLDER")  # You can also write "." instead of "CLUTTERED FOLDER" which means the current directory.
-i=1
-for file in files:
-    if file.endswith(".jpg"):
-        print(file)
-        os.rename(f"CLUTTERED FOLDER/{file}",f"CLUTTERED FOLDER/{i}.jpg")
-        i+=1
