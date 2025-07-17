7f4c9096d050bf7042157462e03bf6fd4b4ffbd8
Mahad Shahnawaz <mahadshahnawaz72@gmail.com>
2025-07-17 23:46:44 +0500

    Delete Request + bs4 Module.py

--- Request + bs4 Module.py
+++
@@ -1,9 +0,0 @@
-import requests                 # "requests" to install it.
-from bs4 import BeautifulSoup   # "bs4" to install it.
-response=requests.get("https://www.google.com")  # This will get me the code of "GOOGLE".
-print(response.text)
-# "html.parser" tells BeautifulSoup how to read and interpret the HTML code, it's like a translator that converts HTML into a structure Python can work with.
-soup=BeautifulSoup(response.text,"html.parser")
-print(soup.prettify())   # The .prettify() method in BeautifulSoup returns a nicely formatted version you're working with.
-for heading in soup.find_all("h2"):  # This tells BeautifulSoup to find all <h2> tags in the HTML document.
-    print("heading.text")
