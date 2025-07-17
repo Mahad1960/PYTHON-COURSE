451d70b876dde8e3d01f5fa093d2658ccd947f18
Mahad Shahnawaz <mahadshahnawaz72@gmail.com>
2025-07-17 23:44:30 +0500

    Add files via upload

---
+++ Copying Files OR Folder, Shutil Module.py
@@ -0,0 +1,4 @@
+# import shutil
+# shutil.copy("SHUTIL MODULE 1.py","SHUTIL Module 2.py")  # This will copy your file in your desired destination.
+# shutil.copytree("PDF FOLDER","COPYTREE FOLDER")  # This will copy entire folder to your destination.
+# shutil.move("COPYTREE FOLDER/MERGED FILES.pdf","MERGED FILES.pdf")  # This step will move "MERGED FILES.pdf" inside "COPYTREE FOLDER" to outside.
