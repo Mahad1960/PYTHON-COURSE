305a558b571edfde2beb781bc1db6af11e14d05a
Mahad Shahnawaz <mahadshahnawaz72@gmail.com>
2025-07-17 23:35:28 +0500

    Delete Pronouncing Text.py

--- Pronouncing Text.py
+++
@@ -1,5 +0,0 @@
-import win32com.client   # "pywin32" to install it.
-# Create voice object
-speaker = win32com.client.Dispatch("SAPI.SpVoice")
-name = input("Enter your name: ")
-speaker.Speak(f"Hello,{name}")
